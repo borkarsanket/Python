6406386d5b9b63769627a650b4869691a96c845e
Sanket <154545900+borkarsanket@users.noreply.github.com>
2024-02-03 17:53:37 +0530

    2.py

---
+++ 2.py
@@ -0,0 +1,13 @@
+#ask user for their name
+name = input("Whats your name? ")
+
+#say hello to user 
+print("hello,")
+print(name)
+
+
+#using variable - name
+#output
+#whats your name? write your name - sanket
+#hello,
+#sanket
