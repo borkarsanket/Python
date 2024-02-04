2a5ea958f3bccc9e23c31ce831fc3c16590753b3
Sanket <154545900+borkarsanket@users.noreply.github.com>
2024-02-04 16:45:36 +0530

    7.py

---
+++ 7.py
@@ -0,0 +1,12 @@
+###condition
+x = int(input("x ? "))
+y = int(input("y ? "))
+
+if x < y:
+    print("x is less than y")
+
+if x > y:
+    print("x is greater than y")
+
+if x == y:
+    print("x is equal to y")
