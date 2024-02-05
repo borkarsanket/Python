0963ea68f395c254fd8abccd078ccb6a23d72608
Sanket <154545900+borkarsanket@users.noreply.github.com>
2024-02-05 17:39:09 +0530

    16

---
+++ 16.py
@@ -0,0 +1,23 @@
+name = input("What is your name? ")
+
+match name:
+    case "sanket":
+        print("Hello SANKET")
+    case "sam":
+        print("Hello SAM")
+    case "rupesh":
+        print("Hello RUPESH")
+    case "yogesh":
+        print("Hello YOGESH")
+    case "shital":
+        print("Hello SHITAL")
+    case "amol":
+        print("Hello AMOL")
+    case "mahesh":
+        print("Hello MAHESH")
+    case "sara":
+        print("Hello SARA")
+    case _:
+        print("Who.....?.......?.....")
+        
+        #### CASE ####
