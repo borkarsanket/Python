a017a2dd0925567f557091282e5eda1e3a34f898
Sanket <154545900+borkarsanket@users.noreply.github.com>
2024-02-05 17:38:48 +0530

    14

---
+++ 14.py
@@ -0,0 +1,17 @@
+name = input("What is your name? ")
+
+if name == "sanket":
+    print("HELLO SANKET")
+elif name == "rupesh":
+    print("HELLO RUPESH")
+elif name == "shital":
+    print("HELLO SHITAL")
+elif name == "sam":
+    print("HELLO SAM")
+elif name == "amol":
+    print("HELLO AMOL")
+else:
+    print("Who are You????????????")
+    
+    
+##### Match == 
