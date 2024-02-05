cc428824a31097887b769f9ac990a46207d05859
Sanket <154545900+borkarsanket@users.noreply.github.com>
2024-02-05 17:38:57 +0530

    15

---
+++ 15.py
@@ -0,0 +1,12 @@
+name = input("What is your name? ")
+
+if name == "sanket" or name == "rupesh" or name == "sam":
+    print("Topper")
+elif name == "Amol":
+    print("Not Topper")
+else:
+    print("data mismatch")
+    
+    
+    
+    #### match ####
