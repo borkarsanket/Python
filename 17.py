ff4e070d5dfbe5af6a59b447ff0b0e3aa4431568
Sanket <154545900+borkarsanket@users.noreply.github.com>
2024-02-07 12:58:24 +0530

    17.py

---
+++ 17.py
@@ -0,0 +1,11 @@
+i = 3
+while i != 0:
+    print("sam")
+    i = i - 1
+    
+    
+    ### while loop 
+    # 3 != 0
+    # 2 != 0
+    # 1 != 0
+    # 0 = 0  forth time its false then it stop ....
