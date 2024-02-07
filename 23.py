6b81cdc813e34a30fdc227e828c4d23ded833d9e
Sanket <154545900+borkarsanket@users.noreply.github.com>
2024-02-07 13:03:53 +0530

    23.py

---
+++ 23.py
@@ -0,0 +1,15 @@
+while True:
+    n = int(input("x ? "))
+    if n > 0:
+        break
+    
+for _ in range(n):
+    print("sanket")
+    
+### output 
+# x ? 5 
+#sanket
+#sanket
+#sanket
+#sanket
+#sanket
