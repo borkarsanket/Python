20608bdd79adf597d293f205f304c5e080492ac9
Sanket <154545900+borkarsanket@users.noreply.github.com>
2024-02-08 17:43:26 +0530

    27.py

---
+++ 27.py
@@ -0,0 +1,12 @@
+students = {
+    "sam":"pune",
+    "meta":"kolhapur",
+    "ssss":"latur"
+}
+
+for student in students:
+    print(student, students[student], sep=", ")
+    
+##sam, pune
+#meta, kolhapur
+#ssss, latur
