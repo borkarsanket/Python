a6c73464d2fcf36c9c0027b69a70ecda5d9e5fe4
Sanket <154545900+borkarsanket@users.noreply.github.com>
2024-02-08 17:43:11 +0530

    26.py

---
+++ 26.py
@@ -0,0 +1,15 @@
+## dict function.
+
+students = {
+    "sam":"pune",
+    "meta":"kolhapur",
+    "ssss":"latur"
+}
+
+print(students["sam"])
+print (students["meta"])
+print(students["ssss"])
+
+#o/p- pune
+#kolhapur
+#latur
