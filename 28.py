7f7469e140b2bbe334a24e12a491b604bb794994
Sanket <154545900+borkarsanket@users.noreply.github.com>
2024-02-08 17:43:37 +0530

    28.py

---
+++ 28.py
@@ -0,0 +1,12 @@
+students  = [
+    {"name": "sanket", "house": "pune"},
+    {"name": "rups", "house": "pune"}
+]
+
+for student in students:
+    print(student["name"], student["house"], sep=",")
+    
+    
+    # 2 DICT USE HERE 
+#sanket,pune
+#rups,pune
