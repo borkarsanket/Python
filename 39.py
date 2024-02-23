9950231de6b835eb5713fd2bb5daf8cf97f4bc90
Sanket <154545900+borkarsanket@users.noreply.github.com>
2024-02-23 19:16:48 +0530

    23/2/2024

---
+++ 39.py
@@ -0,0 +1,16 @@
+myvar = "sam"
+my_var = "rups"
+_my_var = "shital"
+myVar = "sss"
+MYVAR = "eee"
+myvar2 = "ccc"
+
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+
+# variable names types or supported
---
+++ 40.py
@@ -0,0 +1,19 @@
+#convert from int to float:
+x = float(20)
+
+#convert from float to int:
+y = int(12.3)
+
+#convert from int to complex:
+z = complex(3)
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+##Convert from one data type to another data type:
